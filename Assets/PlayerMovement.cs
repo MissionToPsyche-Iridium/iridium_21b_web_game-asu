f926af0208e08d3317ec644e35aa16e9692a01ce
Bobby DeLucia <bdelucia@asu.edu>
2024-09-27 14:57:55 -0700

    Basic Unity Game Prototype from Sprint 1

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        float moveY = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        Vector3 movement = new Vector3(moveX, moveY, 0);
+        transform.position += movement;
+    }
+}
