265544cd6de6890723955c2920bea24fd201c5c5
Fonz <fonz.hamilton@gmail.com>
2024-10-01 18:37:15 -0700

    Fixed merge conflict

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,7 +7,8 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
 
-    void Update() {
+    void Update()
+    {
         float moveX = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
         float moveY = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
 
