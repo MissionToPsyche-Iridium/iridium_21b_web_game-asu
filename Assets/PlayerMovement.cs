103e4e52291064d26d84074d2a298b05d0d93db1
Robert DeLucia Jr. <bdelucia@asu.edu>
2024-10-21 14:45:48 -0700

    Added "jump forward" functionality
    Allows the player to jump forward in any direction by quickly double tapping the desired direction to jump towards

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -11,8 +11,14 @@ public class PlayerMovement : MonoBehaviour
     public float speedBoostMultiplier = 2f;  // speed boost grants double velocity
     public float boostDuration = 2f;         // boost is 2s long
     public float boostCooldown = 2f;         // cooldown is 2s long
-    private bool isBoosting = false;         
-    private bool isOnCooldown = false;       
+    public float dashDistance = 5f;          // how far to jump forward on double press
+    public float doublePressThreshold = 0.3f; // max time between double presses
+
+    private bool isBoosting = false;
+    private bool isOnCooldown = false;
+
+    private float lastPressTimeX = -1f;      // track last press time for X-axis
+    private float lastPressTimeY = -1f;      // track last press time for Y-axis
 
     void Update()
     {
@@ -22,17 +28,43 @@ public class PlayerMovement : MonoBehaviour
         Vector3 movement = new Vector3(moveX, moveY, 0);
         transform.position += movement;
 
-        // if spacebar is pressed and not currently boosting and not on cooldown, start boost
+        // Handle speed boost with spacebar
         if (Input.GetKeyDown(KeyCode.Space) && !isBoosting && !isOnCooldown)
         {
             StartCoroutine(SpeedBoost());
         }
+
+        // Handle double press for "jump forward" on X-axis
+        if (Input.GetButtonDown("Horizontal"))
+        {
+            if (Time.time - lastPressTimeX < doublePressThreshold)
+            {
+                Dash(Vector3.right * Mathf.Sign(Input.GetAxisRaw("Horizontal")));  // Dash in the X direction
+            }
+            lastPressTimeX = Time.time;
+        }
+
+        // Handle double press for "jump forward" on Y-axis
+        if (Input.GetButtonDown("Vertical"))
+        {
+            if (Time.time - lastPressTimeY < doublePressThreshold)
+            {
+                Dash(Vector3.up * Mathf.Sign(Input.GetAxisRaw("Vertical")));  // Dash in the Y direction
+            }
+            lastPressTimeY = Time.time;
+        }
+    }
+
+    // Function to dash the player forward
+    void Dash(Vector3 direction)
+    {
+        transform.position += direction * dashDistance;
     }
 
     IEnumerator SpeedBoost()
     {
         isBoosting = true;
-        moveSpeed *= speedBoostMultiplier; 
+        moveSpeed *= speedBoostMultiplier;
 
         yield return new WaitForSeconds(boostDuration);  // give boost for 2 seconds
 
@@ -48,11 +80,13 @@ public class PlayerMovement : MonoBehaviour
         yield return new WaitForSeconds(boostCooldown);  // cooldown is 2 seconds long
         isOnCooldown = false;
     }
-    void OnTriggerEnter2D(Collider2D other){
-      if(other.gameObject.CompareTag("Coin")){
-        cm.coinCount++;
-        Destroy(other.gameObject);
-      }
-    }
 
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            cm.coinCount++;
+            Destroy(other.gameObject);
+        }
+    }
 }
