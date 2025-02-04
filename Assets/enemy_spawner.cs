fbdf5c7df2b60d589b122a7ef280cc9e3b9249ae
jcalle19 <jcalle19@asu.edu>
2025-02-04 13:18:53 -0700

    Changed spawner behavior
    Spawner no longer spawns enemies off screen due to size of stage, but enemies no longer spawn out of bounds

--- Assets/enemy_spawner.cs
+++ Assets/enemy_spawner.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,10 @@ public class enemy_spawner : MonoBehaviour
 {
     public GameObject Player;
     public GameObject Enemy;
+    public float levelMaxY;
+    public float levelMinY;
+    public float levelMaxX;
+    public float levelMinX;
     private float randTimer = 0;
     private float randSpawnrate = .7f;
     private float randX;
@@ -33,13 +38,18 @@ public class enemy_spawner : MonoBehaviour
         }
         else if (numEnemies < 20)
         {
-            randXNeg = (float)Random.Range(-12, -6) + Player.transform.position.x;
-            randYNeg = (float)Random.Range(-8, -4) + Player.transform.position.y;
+            /*
+            randXNeg = (float)Random.Range(-12, -6) + Player.transform.position.x; //was -12
+            randYNeg = (float)Random.Range(-8, -4) + Player.transform.position.y; //was -8
             randXPos = (float)Random.Range(6, 12) + Player.transform.position.x;
             randYPos = (float)Random.Range(4, 8) + Player.transform.position.y;
-
-            randX = Random.Range(0,2) == 1 ? randXNeg : randXPos;
-            randY = Random.Range(0,2) == 1 ? randYNeg : randYPos;
+            */
+            randXNeg = (float)UnityEngine.Random.Range(levelMinX + Math.Abs(Player.transform.position.x), Player.transform.position.x);
+            randYNeg = (float)UnityEngine.Random.Range(levelMinY + Math.Abs(Player.transform.position.y), Player.transform.position.y);
+            randXPos = (float)UnityEngine.Random.Range(Player.transform.position.x - Math.Abs(Player.transform.position.x), levelMaxX);
+            randYPos = (float)UnityEngine.Random.Range(Player.transform.position.y - Math.Abs(Player.transform.position.y), levelMaxY);
+            randX = UnityEngine.Random.Range(0,2) == 1 ? randXNeg : randXPos;
+            randY = UnityEngine.Random.Range(0,2) == 1 ? randYNeg : randYPos;
 
             Debug.Log(randX + " " + randY);
             Instantiate(Enemy, new Vector3(randX, randY, 1), transform.rotation);
