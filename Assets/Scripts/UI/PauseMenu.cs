d98b5a31af2ddf832d9c83e1c127fd8c95fa4e2b
jcalle19 <jcalle19@asu.edu>
2025-04-29 20:54:07 -0700

    number of boss segments now resets upon restart
    made it so the number of boss segments resets to 5 when the player restarts the game

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -30,6 +30,7 @@ public class PauseMenu : MonoBehaviour
         dash_enemy_script.speedFactor = 1.0f;
         shoot_enemy_behavior.speedFactor = 1.0f;
         head_behavior.speedFactor = 1.0f;
+        head_behavior.numSegments = 5;
         body_follow.speedFactor = 1.0f;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); // Reload current scene
     }
