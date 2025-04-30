9c8e00cd07acc213d6a0dadcebdd42abc6010d2f
jcalle19 <jcalle19@asu.edu>
2025-04-29 20:46:57 -0700

    Fixed metal drops and scaling on reset
    made it so that the metals dropping and the enemy's scaled stats reset upon game restart

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -24,6 +24,13 @@ public class PauseMenu : MonoBehaviour
     public void RestartGame()
     {
         Time.timeScale = 1f; // Ensure time resumes
+        EnemyHealth.numMetals = 1;
+        EnemyHealth.healthScale = 1.0f;
+        basic_enemy_behavior.speedFactor = 1.0f; //no speed scaling
+        dash_enemy_script.speedFactor = 1.0f;
+        shoot_enemy_behavior.speedFactor = 1.0f;
+        head_behavior.speedFactor = 1.0f;
+        body_follow.speedFactor = 1.0f;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); // Reload current scene
     }
 
